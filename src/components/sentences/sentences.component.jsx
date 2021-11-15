b59440df16e9063ff434a7130da215873f9ab48a
andy <axistation@gmail.com>
2021-11-15 22:31:24 +0800

    test commit

--- src/components/sentences/sentences.component.jsx
+++ src/components/sentences/sentences.component.jsx
@@ -1,16 +1,16 @@
-import React from "react";
+import React from 'react';
 
-import Container from "react-bootstrap/Container";
+import Container from 'react-bootstrap/Container';
 
-import Sentence from "../sentence/sentence.component";
+import Sentence from '../sentence/sentence.component';
 
-import "./sentences.styles.scss";
+import './sentences.styles.scss';
 
 const Sentences = ({ title, sentences }) => (
-  <Container className="main ma mt-3">
-    <h5 className="title text-center">{title}</h5>
+  <Container className='main ma mt-3'>
+    <h5 className='title text-center'>{title} test</h5>
     {sentences.map((sentence) => (
-      <div key={sentence.id} className="sentences">
+      <div key={sentence.id} className='sentences'>
         <Sentence morphs={sentence.morphs} translated={sentence.translated} />
       </div>
     ))}
