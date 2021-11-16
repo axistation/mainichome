2ddcb64ce9d01255c0f39de279beeb5bfaafe895
andy <axistation@gmail.com>
2021-11-16 18:50:21 +0800

    fix bugs

--- src/components/sentences/sentences.component.jsx
+++ src/components/sentences/sentences.component.jsx
@@ -8,7 +8,7 @@ import './sentences.styles.scss';
 
 const Sentences = ({ title, sentences }) => (
   <Container className='main ma mt-3'>
-    <h5 className='title text-center'>{title} test</h5>
+    <h5 className='title text-center'>{title}</h5>
     {sentences.map((sentence) => (
       <div key={sentence.id} className='sentences'>
         <Sentence morphs={sentence.morphs} translated={sentence.translated} />
--- src/components/stories/stories.component.jsx
+++ src/components/stories/stories.component.jsx
@@ -1,21 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from 'react';
 
-import "./stories.styles.scss";
+import './stories.styles.scss';
 
-import STORIES_DATA from "./stories.data.js";
+import Story from '../story/story.component';
 
-import Story from "../story/story.component";
+import { getContentData } from '../content/content.data';
 
 function Stories() {
-  const [sections] = useState(STORIES_DATA);
+  const [sections, setSections] = useState([]);
   const [activeId, setActiveId] = useState();
 
+  useEffect(() => {
+    (async () => {
+      setSections(await getContentData());
+    })();
+  }, []);
+
   return (
-    <div className="stories">
+    <div className='stories'>
       {sections.map(({ id, ...otherSectionProps }) => (
         <div
           onClick={() => setActiveId(id)}
-          className={`story ${activeId === id ? "activeStory" : ""}`}
+          className={`story ${activeId === id ? 'activeStory' : ''}`}
           key={id}
         >
           <Story {...otherSectionProps} />
