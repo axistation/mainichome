1e1ac164fbe7892ece0be3b6fdc5675e008b4bc8
andy <axistation@gmail.com>
2021-11-17 20:24:45 +0800

    add story

--- src/components/content/content.data.stories.js
+++ src/components/content/content.data.stories.js
@@ -1,10 +1,17 @@
 function train() {
   return fetch(
-    'https://axistation.github.io/mainichome-data/content.data.stories/train.json'
+    'https://axistation.github.io/mainichome-data/content.data.stories/電車 - Train.json'
+  ).then((response) => response.json());
+}
+
+function theNorthWindAndTheSun() {
+  return fetch(
+    'https://axistation.github.io/mainichome-data/content.data.stories/北風と太陽 - The North Wind and the Sun.json'
   ).then((response) => response.json());
 }
 
 const contentDataStories = [];
 contentDataStories.push(train);
+contentDataStories.push(theNorthWindAndTheSun);
 
 export default contentDataStories;
