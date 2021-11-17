c61b4ca76d6e32d17abc46fa12e5051fa29185e9
andy <axistation@gmail.com>
2021-11-17 20:40:14 +0800

    logo

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,26 +1,26 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-import "./header.styles.scss";
-import Logo from "../../assets/logo.svg";
+import './header.styles.scss';
+import Logo from '../../assets/logo.svg';
 
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import Container from "react-bootstrap/Container";
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
 
-import Sidebar from "../../components/sidebar/sidebar.component";
+import Sidebar from '../../components/sidebar/sidebar.component';
 
 const Header = () => (
-  <Navbar className="px-4" bg="light" expand="lg" sticky="top">
+  <Navbar className='px-4' bg='light' expand='lg' sticky='top'>
     <Container>
       <Navbar.Brand>
-        <Link className="logo-container" to="/">
-          <img src={Logo} alt="logo" height="24" />
-          <span className="px-2">mainichome</span>
+        <Link className='logo-container' to='/'>
+          <img src={Logo} alt='logo' height='24' />
+          <span className='px-2'>mainichōme</span>
         </Link>
       </Navbar.Brand>
 
-      <Nav className="justify-content-end">
+      <Nav className='justify-content-end'>
         <Sidebar />
       </Nav>
     </Container>
