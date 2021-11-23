e583a1f3be362bff28d50539d49cf7adea73b675
andy <axistation@gmail.com>
2021-11-23 19:35:38 +0800

    logo

--- src/components/sentence/sentence.component.jsx
+++ src/components/sentence/sentence.component.jsx
@@ -1,37 +1,37 @@
-import React from "react";
+import React from 'react';
 
-import OverlayTrigger from "react-bootstrap/OverlayTrigger";
-import Popover from "react-bootstrap/Popover";
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Popover from 'react-bootstrap/Popover';
 
-import "./sentence.styles.scss";
+import './sentence.styles.scss';
 
 const Sentence = ({ morphs, translated }) => (
-  <div className="sentence-group">
-    <div className="sentence-japanese">
-      <div className="morphs">
+  <div className='sentence-group'>
+    <div className='sentence-japanese'>
+      <div className='morphs'>
         {morphs.map((morph) => (
           <span key={morph.id}>
             <OverlayTrigger
-              key="bottom"
-              placement="bottom"
+              key='top'
+              placement='top'
               overlay={
                 <Popover>
-                  <Popover.Header className="popover-header">
-                    <span className="kana">{morph.kana}</span> /{" "}
-                    <span className="romaji">{morph.romaji}</span>
+                  <Popover.Header className='popover-header'>
+                    <span className='kana'>{morph.kana}</span> /{' '}
+                    <span className='romaji'>{morph.romaji}</span>
                   </Popover.Header>
-                  <Popover.Body className="popover-body">
+                  <Popover.Body className='popover-body'>
                     {morph.meaning}
                   </Popover.Body>
                 </Popover>
               }
             >
-              <span className="morph">{morph.name}</span>
+              <span className='morph'>{morph.name}</span>
             </OverlayTrigger>
           </span>
         ))}
       </div>
-      <div className="sentence-translated">{translated}</div>
+      <div className='sentence-translated'>{translated}</div>
     </div>
   </div>
 );
